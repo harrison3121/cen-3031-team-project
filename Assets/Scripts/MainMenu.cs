e9e0a0c8f4f05c8c74f6a5ea4eafc74ddae5e141
harrison3121 <126530603+harrison3121@users.noreply.github.com>
2024-12-02 11:12:03 -0500

    Combined all Levels

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,20 +4,37 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
     public Button world2Button;
     public void StartLevel1()
     {
         SceneManager.LoadScene("Level1Lesson");
     }
     
+    public void StartLevel2()
+    {
+        SceneManager.LoadScene("Level2Lesson");
+    }
+    public void StartLevel3()
+    {
+        SceneManager.LoadScene("Level3Lesson");
+    }
+    public void StartLevel4()
+    {
+        SceneManager.LoadScene("Level4Lesson");
+    }
+    public void StartLevel5()
+    {
+        SceneManager.LoadScene("Level5Lesson");
+    }
+
     void Start(){
-        if (PlayerPrefs.GetInt("World2Unlocked", 0) == 1)
-        {
-            world2Button.interactable = true; 
-        }
-        else
-        {
-            world2Button.interactable = false; 
+        world2Button.gameObject.SetActive(false);
+        Debug.Log("World2Unlocked: " + PlayerPrefs.GetInt("World2Unlocked", 0));
+    }
+    void Update(){
+        if(PlayerPrefs.GetInt("World2Unlocked", 0) == 1){
+            world2Button.gameObject.SetActive(true);
         }
     }
 
--- Assets/Scripts/ToMainMenu.cs
+++ Assets/Scripts/ToMainMenu.cs
@@ -4,8 +4,32 @@ using UnityEngine.SceneManagement;
 public class ToMainMenu : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public void MainMenu()
+    public void MainMenu1()
     {
+        PlayerPrefs.SetInt("World2Unlocked", 1);
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void MainMenu2()
+    {
+        PlayerPrefs.SetInt("World3Unlocked", 1);
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void MainMenu3()
+    {
+        PlayerPrefs.SetInt("World4Unlocked", 1);
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void MainMenu4()
+    {
+        PlayerPrefs.SetInt("World5Unlocked", 1);
+        SceneManager.LoadScene("Menu");
+    }
+    public void MainMenu5()
+    {
+        PlayerPrefs.SetInt("Done", 1);
         SceneManager.LoadScene("Menu");
     }
 }
